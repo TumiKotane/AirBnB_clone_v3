2f66df84448cfba5b4585626f6d650404d0ef63d
ThexdxrBlck <morobe76o@gmail.com>
2023-10-10 08:00:17 -0700

    updated but not done console.py

--- console.py
+++ console.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 """ console """
+
+imprt cmd
+from datatime import datetime
+from models
+from models.city import City
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.user import User
+import shlex
