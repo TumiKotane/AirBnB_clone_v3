06c504918a26dcf31601a9c3e53bce475c3e3328
TumiKotane <kaiineoyoung@gmail.com>
2024-01-08 15:39:38 -0800

    Task 2

--- models/__init__.py
+++ models/__init__.py
@@ -11,9 +11,12 @@ from models.place import Place
 from models.review import Review
 from os import getenv
 
+storage_t = getenv("HBNB_TYPE_STORAGE")
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
